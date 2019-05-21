faaad980b1651022c83be1a680c8f174cd747e7b
alicja <pajdaalicja@gmail.com>
2019-05-21 19:41:30 +0200

    initial commit

---
+++ app/src/Entity/Category.php
@@ -0,0 +1,192 @@
+<?php
+/**
+ * Category entity.
+ */
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Class Category.
+ *
+ * @ORM\Entity(repositoryClass="App\Repository\CategoryRepository")
+ * @ORM\Table(name="categories")
+ *
+ * @UniqueEntity(fields={"title"})
+ */
+class Category
+{
+    /**
+     * Use constants to define configuration options that rarely change instead
+     * of specifying them in app/config/config.yml.
+     * See http://symfony.com/doc/current/best_practices/configuration.html#constants-vs-configuration-options
+     *
+     * @constant int NUMBER_OF_ITEMS
+     */
+    const NUMBER_OF_ITEMS = 10;
+
+    /**
+     * Primary key.
+     *
+     * @var int
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * Created at.
+     *
+     * @var \DateTime
+     *
+     * @Gedmo\Timestampable(on="create")
+     *
+     * @ORM\Column(type="datetime")
+     *
+     * @Assert\DateTime
+     */
+    private $createdAt;
+
+    /**
+     * Updated at.
+     *
+     * @var \DateTime
+     *
+     * @Gedmo\Timestampable(on="update")
+     *
+     * @ORM\Column(type="datetime")
+     *
+     * @Assert\DateTime
+     */
+    private $updatedAt;
+
+    /**
+     * Title.
+     *
+     * @var string
+     *
+     * @ORM\Column(
+     *     type="string",
+     *     length=64
+     * )
+     *
+     * @Assert\NotBlank
+     * @Assert\Length(
+     *     min="3",
+     *     max="64",
+     * )
+     */
+    private $title;
+
+    /**
+     * Tasks.
+     *
+     * @var Collection|null
+     *
+     * @ORM\OneToMany(
+     *     targetEntity="App\Entity\Task",
+     *     mappedBy="category",
+     * )
+     */
+    private $tasks;
+
+    /**
+     * Code.
+     *
+     * @var string
+     *
+     * @ORM\Column(
+     *     type="string",
+     *     length=64
+     * )
+     *
+     * @Gedmo\Slug(fields={"title"})
+     *
+     * @Assert\Length(
+     *     min="3",
+     *     max="64",
+     * )
+     */
+    private $code;
+
+
+    /**
+     * Getter for Id.
+     *
+     * @return int|null Id
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Getter for Created at.
+     *
+     * @return \DateTimeInterface|null Created at
+     */
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Setter for Created at.
+     *
+     * @param \DateTimeInterface $createdAt Created at
+     */
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * Getter for Updated at.
+     *
+     * @return \DateTimeInterface|null Updated at
+     */
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * Setter for Updated at.
+     *
+     * @param \DateTimeInterface $updatedAt Updated at
+     */
+
+    public function setUpdatedAt(\DateTimeInterface $updatedAt): void
+    {
+        $this->updatedAt = $updatedAt;
+    }
+
+    /**
+     * Getter for Title.
+     *
+     * @return string|null Title
+     */
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    /**
+     * Setter for Title.
+     *
+     * @param string $title Title
+     */
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+}
